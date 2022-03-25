65653e88fc623a7dbd60c534f79f0f6b7aad71a5
hyeyeonguim <102298862+hyeyeonguim@users.noreply.github.com>
2022-03-25 21:43:07 +0900

    220325

--- js/main.js
+++ js/main.js
@@ -1,61 +1,71 @@
 $(function () {
-    /////////////////////////////////////////////////////// 
+    /////////////////////////////
+    //topBanner 2022.03.17 최종수정 hyeyeonk
+    //$('css선택자').on('이벤트',할일);
 
-    // topBanner 2022.03.17 최종수정 JJAMMIN 
-    // $('css선택자').on('이벤트' , 할일)
+
+    //$('.topBanner i').on('click', ()=>$('.topBanner').slideUp());
+
+
+    $('.topBanner i').on('click', topbannerHandler);
 
     function topbannerHandler() {
+        //$('.topBanner').hide();
         $('.topBanner').slideUp();
         곱하기(10, 20);
     }
 
-    $('.topBanner i ').on('click', topbannerHandler);
+    function 곱하기(a, b) {
+        console.log(a * b);
+    }
+
+    function 오오오(e) {
+        //e.preventDefault(); //a태그 새로고침 안됨
+        const tg = e.target;
+        console.log(tg, '오오오 클랙했네!');
+    }
+
+    $('.ooooo').on('click', 오오오);
 
 
 
-    $('.mainVisualSlide').on('init reInit afterChange', function () {
+    //** 각각의 슬라이드에 애니메이션
+    /////////////////////////////
+    // 슬릭에 있는 이벤트 : init reinint afterChange 이벤트 두개이상일때 두개다 실행
+    $('.mainVisualSlide').on('init reinit afterChange', function () {
         let current = $('.slick-current');
         current.addClass('on').siblings().removeClass('on');
+        //메소드 체인
     });
 
-
+    //메인슬라이드
     $('.mainVisualSlide').slick({
         autoplay: true,
-        autoplaySpeed: 2000,
+        autoplaySpeed: 3000,
         arrows: false,
         pauseOnHover: false,
-        pauseOnFocus: false,
+        pauseOnFocus: false
     });
 
     //$('.mainVisualSlide figure').eq(1).addClass('on');
-    // //전체 슬라이드에서 class를 떄고 지금 스라이드에 class 붙이는 일
+    //전체 슬라이드에서 클래스를 떼고 지금 슬라이드에 클래스를 붙임
 
-    // $('.mainVisualSlide').on('afterChange', function (s, i, c) {
-    //     //c => 0,1,2
-    //     $('.mainVisualSlide figure').removeClass('on')
-    //     $('.mainVisualSlide figure').eq(c + 1).addClass('on');
-    // });
-
-
-
+    //$('.mainVisualSlide').on('afterChange', function (s, i, c) {
+    //console.log(s, i, c); 
+    //c => 0,1,2
+    //$('.mainVisualSlide figure').removeClass('on');
+    //$('.mainVisualSlide figure').eq(c + 1).addClass('on'); //1,2,3
+    //});
 
 
+    // 동영상
+    $('.movieBtn i:first-child').on('click', function () {
+        $('#adMovie').trigger('pause');
+    });
+    $('.movieBtn i:last-child').on('click', function () {
+        $('#adMovie').trigger('play');
+    });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    /////////////////////////////////////////////////////
+    /////////////////////////////
 });
--- js/popup.js
+++ js/popup.js
@@ -1,8 +1,22 @@
+// 자바스크립트
+// //구조가 만들어진 다음 실행하라
+// window.addEventListener('DOMContentLoaded', ()=>{
+//     document.querySelector('h1').style.color='#f00';
+//     document.querySelector('h1').style.fontSize= 100 + "px";
+// })
+
+
+//제이쿼리(자바스크립트 라이브러리)
 $(function(){
-    function popupClose() {
-        $(this).parent().hide();
+    //$('h1').css({color:'#f00', fontSize:'100px', display:'none'});
+    //$('h1').hide();
+
+    function popupClose(){
+        //console.log(this)
+        //$('.popup').hide();
+        $(this).parent().hide(); //팝업이 여러개일경우, 나의 부모태그를 닫아주므로 *매우 효율적*
     }
     $('.btn').on('click', popupClose);
-
 })
+
 
